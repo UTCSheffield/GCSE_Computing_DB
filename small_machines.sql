4a9e3411b4c491fa2e392070c8f0376d4be43363
Mr Eggleton <meggleton@utcsheffield.org.uk>
2015-06-01 15:33:26 +0100

    Update small_machines.sql

--- small_machines.sql
+++ small_machines.sql
@@ -1,7 +1,7 @@
 create table stock (stockid INTEGER PRIMARY KEY ASC, name STRING, max INTEGER, taken INTEGER);
-insert into stock (name, max, taken) VALUES ("Crawler", 50, 0);
-insert into stock (name, max, taken) VALUES ("Hoist", 50, 0);
-insert into stock (name, max, taken) VALUES ("Mini Digger", 50, 0);
-insert into stock (name, max, taken) VALUES ("Turntable", 50, 0);
+insert into stock (name, max, taken) VALUES ("Crawler", 10, 0);
+insert into stock (name, max, taken) VALUES ("Hoist", 20, 0);
+insert into stock (name, max, taken) VALUES ("Mini Digger", 30, 0);
+insert into stock (name, max, taken) VALUES ("Turntable", 40, 0);
 insert into stock (name, max, taken) VALUES ("Platform Lift", 50, 0);
 select * from stock;
