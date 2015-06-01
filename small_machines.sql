59a9fca8e8b531290ddc654343b128ebf2f5427a
Mr Eggleton <meggleton@utcsheffield.org.uk>
2015-06-01 15:29:27 +0100

    Create small_machines.sql

---
+++ small_machines.sql
@@ -0,0 +1,6 @@
+create table stock (stockid INTEGER PRIMARY KEY ASC, name STRING, max INTEGER, taken INTEGER);
+insert into stock (name, max, taken) VALUES ("Crawler", 50, 0);
+insert into stock (name, max, taken) VALUES ("Hoist", 50, 0);
+insert into stock (name, max, taken) VALUES ("Mini Digger", 50, 0);
+insert into stock (name, max, taken) VALUES ("Turntable", 50, 0);
+insert into stock (name, max, taken) VALUES ("Platform Lift", 50, 0);
