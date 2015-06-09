7b3cda201c1591365262639c4b50c4e2cc46adcb
Mr Eggleton <meggleton@utcsheffield.org.uk>
2015-06-09 16:22:16 +0000

    properly Relational

--- routes/stock.js
+++ routes/stock.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var router = express.Router();
-//var hbs = require("handlebars");
+var Handlebars = require("hbs");
 var sqlite3 = require('sqlite3').verbose();
 var db;
 
@@ -13,20 +13,56 @@ function createDb(fAction) {
 function getAll(res) {
 	var sSQL = "SELECT stock.stockid, stock.name as name, stock.max, SUM(sales.quantity) as taken FROM stock LEFT JOIN sales ON(stock.stockid = sales.stockid) GROUP BY stock.stockid;";
 	db.all( sSQL, function(err, rows) {
-		var sOut = "<html><head><style>div table {font-size:200%;}</style></head><body><h1>Current Stock</h1><div>"+sSQL+"</div>";
-		sOut = sOut + "<table><tr><th>Stockid</th><th>Name</th><th>Max</th><th>Taken</th></tr>";
-		var aOut = rows.map(function(row){
-			return "<tr><td>"+row.stockid+"</td><td>"+row.name+"</td><td>"+row.max+"</td><td>"+row.taken+"</td></tr>";
+		rows = rows.map(function(row){
+		    if(!row.taken) {
+		        row.taken = 0;
+		    }
+		    return row;
 		});
-
-		sOut = sOut+aOut.join("\n")+"</table></body></html>";
-		res.send(sOut);
+		res.render('stock_list', {
+		    title:"Current Stock",
+		    sSQL:sSQL,
+		    rows:rows
+		});
 		closeDb();
 	});
 }
 
+function getSales(res) {
+	var sSQL = "SELECT * FROM sales;";
+	db.all( sSQL, function(err, rows) {
+		res.render('sales_list', {
+		    title:"Sales List",
+		    sSQL:sSQL,
+		    rows:rows
+		});
+		closeDb();
+	});
+}
+
+function getSalesReport(res) {
+	var sSQL = "SELECT users.username, stock.name, SUM(sales.quantity) as quantity FROM stock JOIN sales ON (stock.stockid = sales.stockid) JOIN users ON (users.userid = sales.userid) GROUP BY  users.username, stock.name;";
+	db.all( sSQL, function(err, rows) {
+		res.render('sales_report', {
+		    title:"Sales Report",
+		    sSQL:sSQL,
+		    rows:rows
+		});
+		closeDb();
+	});
+}
+
+
 function getAllJson(res) {
-	db.all("SELECT * FROM stock", function(err, rows) {
+	var sSQL = "SELECT stock.stockid, stock.name as name, stock.max, SUM(sales.quantity) as taken FROM stock LEFT JOIN sales ON(stock.stockid = sales.stockid) GROUP BY stock.stockid;";
+	db.all(sSQL, function(err, rows) {
+	    rows = rows.map(function(row){
+		    if(!row.taken) {
+		        row.taken = 0;
+		    }
+		    return row;
+		});
+		
 		var sOut = JSON.stringify(rows, null, 4);
 		res.send(sOut);
 		closeDb();
@@ -43,8 +79,17 @@ function getMaxes(res) {
 }
 
 function getTaken(res) {
-        db.all("SELECT * FROM stock", function(err, rows) {
-                var aOut = rows.map(function(item){return item.taken;});
+    var sSQL = "SELECT stock.stockid, stock.name as name, stock.max, SUM(sales.quantity) as taken FROM stock LEFT JOIN sales ON(stock.stockid = sales.stockid) GROUP BY stock.stockid;";
+	db.all(sSQL, function(err, rows) {
+	        var aOut = rows.map(function(item){
+                        
+                    if(!item.taken) {
+                        item.taken = 0;
+                    }
+                    
+                    return item.taken;
+                });
+                
                 var sOut = JSON.stringify(aOut);
                 res.send(sOut);
                 //closeDb();
@@ -54,15 +99,19 @@ function getTaken(res) {
 
 function book(res, req) {
 	console.log(req);
-	var id = req.query.id ;
-	db.run("UPDATE stock SET taken = taken + 1 WHERE stockid=(?)", id);
+	var id = req.query.stockid;
+	var userid = req.query.userid;
+	
+	db.run("INSERT INTO sales (userid, stockid, quantity) VALUES ((?), (?), 1);", userid, id);
   	getTaken(res);
 }
 
 function cancel(res, req) {
-	var id = req.query.id ;
-	db.run("UPDATE stock SET taken = taken - 1 WHERE stockid=(?)", id);
-  	getTaken(res);
+	var id = req.query.stockid;
+	var userid = req.query.userid;
+	db.run("INSERT INTO sales (userid, stockid, quantity) VALUES ((?), (?), -1);", userid, id);
+  	
+	getTaken(res);
 }
 
 
@@ -72,6 +121,14 @@ function closeDb() {
 
 router.get('/', function(req, res, next) {
    	createDb(getAll.bind(this, res));
+});
+
+router.get('/sales', function(req, res, next) {
+   	createDb(getSales.bind(this, res));
+});
+
+router.get('/salesreport', function(req, res, next) {
+   	createDb(getSalesReport.bind(this, res));
 });
 
 router.get('/max', function(req, res, next) {
