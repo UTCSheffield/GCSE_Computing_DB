773c430747d97a382a68b109e7e7aff7d995e8a0
Martyn Eggleton <martyn.eggleton@gmail.com>
2015-06-10 14:19:15 +0100

    Fixes #1 #2 #3 #5 #7

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+var hbs = require('hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
--- small_machines.sql
+++ small_machines.sql
@@ -1,15 +1,14 @@
 DROP TABLE stock; 
 CREATE TABLE stock (
-    stockid INTEGER PRIMARY KEY ASC, 
+    stockid INTEGER PRIMARY KEY ASC, -- ASC = Ascending, starts at 1 and goes up each time 
     name STRING, 
     max INTEGER
 );
-INSERT INTO stock (name, max) VALUES ("Crawler", 10);
+INSERT INTO stock (name, max) VALUES ("Crawler", 10); -- We let the ASC bit workout the stockid for us
 INSERT INTO stock (name, max) VALUES ("Hoist", 20);
 INSERT INTO stock (name, max) VALUES ("Mini Digger", 30);
 INSERT INTO stock (name, max) VALUES ("Turn Table", 40);
 INSERT INTO stock (name, max) VALUES ("Platform Lift", 50);
-SELECT * FROM stock; --Select all the fields from every record
 
 DROP TABLE users;
 CREATE TABLE users (
@@ -17,8 +16,7 @@ CREATE TABLE users (
     username STRING, 
     fullname STRING
 );
-INSERT INTO users (username) VALUES ("test");
-SELECT * FROM users;
+INSERT INTO users (username) VALUES ("test", "Mr Trevor Test");
 
 DROP TABLE sales;
 CREATE TABLE sales (  saleid INTEGER PRIMARY KEY ASC,
@@ -28,13 +26,4 @@ CREATE TABLE sales (  saleid INTEGER PRIMARY KEY ASC,
     );
                         
 INSERT INTO sales (userid, stockid, quantity)  VALUES (1, 1, 8);
-SELECT * FROM sales;
 
-SELECT 
-    stock.stockid, 
-    stock.name as name, 
-    stock.max, 
-    SUM(sales.quantity) as sold, 
-    stock.max - SUM(sales.quantity) as available 
-FROM sales JOIN stock ON(stock.stockid = sales.stockid)  
-GROUP BY stock.stockid;
