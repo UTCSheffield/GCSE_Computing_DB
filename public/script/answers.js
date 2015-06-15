5adf91e2ea812c39873cf9d9fdd779cda527960b
Martyn Eggleton <martyn.eggleton@gmail.com>
2015-06-15 05:04:41 +0100

    Adds persistance of presenatation sate and mouse click advancing

--- public/script/answers.js
+++ public/script/answers.js
@@ -1,26 +1,63 @@
-$( document ).ready(function(){
-    $("#start_presentation").click(function(){
+$( document ).ready(function() {
+    
+    var answer = -1;
+        
+    var nextQuestion = function() {
+        if(answer >= 0) {
+            var eOld = $(".answer").eq(answer);
+            eOld.parent().removeClass("current");
+        }
+        answer ++;
+        //answer = answer % $(".answer").length; // Loops
+        answer = Math.min(answer , $(".answer").length - 1);
+        var eAnswer = $(".answer").eq(answer);
+        eAnswer.show();
+        eAnswer.addClass("answered");
+        eAnswer.parent().addClass("current");
+        
+        var fTop = eAnswer.offset().top;
+        var fTarget = Math.max(0, fTop + (eAnswer.height()/2) - ($( window ).height() / 2));
+        $(document).scrollTop(fTarget);
+    };
+        
+    var startPresentation = function() {
         $(".answer").hide();
         $("body").addClass("presentation");
-        var answer = -1;
+        
         $(document).scrollTop(0);
         $("body").keypress(function(event){
             if(event.key === "`") {
-                if(answer >= 0) {
-                    var eOld = $(".answer").eq(answer);
-                    eOld.parent().removeClass("current");
-                }
-                answer ++;
-                answer = answer % $(".answer").length;
-                var eAnswer = $(".answer").eq(answer);
-                eAnswer.show();
-                eAnswer.addClass("answered");
-                eAnswer.parent().addClass("current");
-                
-                var fTop = eAnswer.offset().top;
-                var fTarget = Math.max(0, fTop + (eAnswer.height()/2) - ($( window ).height() / 2));
-                $(document).scrollTop(fTarget);
+                nextQuestion();
             }
+        }).click(function(event){
+            nextQuestion();
         });
-    });
+    };
+    
+    var stopPresentation = function() {
+        $(".answer").show();
+        $("body").removeClass("presentation");
+        $(".current").removeClass("current");
+        $(".answered").removeClass("answered");
+    };        
+    
+    var togglePresentation = function() {
+        if(localStorage.getItem("sPresentation") === "Started") {
+            localStorage.setItem("sPresentation", "Stopped");
+            stopPresentation();
+            $("#start_presentation").html("Start Presentation Mode");
+        } else {
+            localStorage.setItem("sPresentation", "Started");
+            startPresentation();
+            $("#start_presentation").html("Exit Presentation Mode");
+        }
+    };
+        
+    if(localStorage.getItem("sPresentation") === "Started") {
+        startPresentation();
+        $("#start_presentation").html("Exit Presentation Mode");
+    }
+    
+    $("#start_presentation").click(togglePresentation);
+    
 });
